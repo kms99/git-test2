3546bd4203593dcc5d3de6ebb68ffbcec0b40b5b
Kim <audtjq@naver.com>
2019-03-20 15:29:57 +0900

    Initial Commit2

--- app/src/main/java/com/example/gitprac320/MainActivity.java
+++ app/src/main/java/com/example/gitprac320/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
     }
